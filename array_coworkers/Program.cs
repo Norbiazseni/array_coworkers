6934d8baef73b6bab5033543a392dff9ce0655b6
Norbiazseni <131431630+Norbiazseni@users.noreply.github.com>
2024-03-25 11:19:09 +0100

    Add project files.

---
+++ array_coworkers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_coworkers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Norbi: tömb létrehozása
+            //Adrián: tömb adatainak összeadása
+            //x: tömb eredmények kiírása
+        }
+    }
+}
